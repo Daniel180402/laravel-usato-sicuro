e12f09977bf33fe86d2497ebe7f03c5b731adeb3
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-05-24 09:33:14 +0200

    minor fixes

--- public/js/app.js
+++ public/js/app.js
@@ -53795,8 +53795,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/manuelgerolin/ManuelCorsi/Boolean55/esercizi/laravel-usato-sicuro/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/manuelgerolin/ManuelCorsi/Boolean55/esercizi/laravel-usato-sicuro/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\danie\Desktop\Boolean\PHP\laravel-usato-sicuro-2\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\danie\Desktop\Boolean\PHP\laravel-usato-sicuro-2\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
